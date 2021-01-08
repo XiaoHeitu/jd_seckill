d25ad1dfa39ef5ae745f0f5ac9ef1e0ea143370f
小黑兔 <173592829@qq.com>
2021-01-08 22:48:34 +0800

    1

--- main.py
+++ main.py
@@ -21,12 +21,10 @@ if __name__ == '__main__':
     print(a)
 
     jd_seckill = JdSeckill()
-	choice_function = '0'
 	if len(sys.argv) == 1:	
 		choice_function = input('请选择:')
 	else:
-		choice_function = sys.argv[1]
-		
+		choice_function = sys.argv[1]		
     if choice_function == '1':
         jd_seckill.reserve()
     elif choice_function == '2':
